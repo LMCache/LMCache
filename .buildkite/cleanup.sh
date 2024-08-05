c625c7a2deb8c0470a6c85219dedff37a967c46a
ApostaC <jc4xvyp@outlook.com>
2024-08-05 17:50:19 -0500

    adding better cleanup

--- .buildkite/cleanup.sh
+++ .buildkite/cleanup.sh
@@ -6,5 +6,5 @@ PYTHON_VERSION=3.10
 eval "$(conda shell.bash hook)"
 conda activate ${CONDA_ENV_NAME}
 
-pip uninstall lmcache
-pip uninstall torchac_cuda
+pip uninstall -y lmcache
+pip uninstall -y torchac_cuda
