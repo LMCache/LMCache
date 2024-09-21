e17cfab63bf63ca70935826227c16016855674fa
Xueshen Liu <liuxs@umich.edu>
2024-09-21 15:17:21 -0400

    Update fast_serde.py
    Add a tag to specify data type

--- lmcache/storage_backend/serde/fast_serde.py
+++ lmcache/storage_backend/serde/fast_serde.py
@@ -1,6 +1,7 @@
 import torch
 import io
 import time
+import numpy as np
 
 from lmcache.storage_backend.serde.serde import Serializer, Deserializer
 from lmcache.logging import init_logger
@@ -8,21 +9,34 @@ from lmcache.config import GlobalConfig
 
 logger = init_logger(__name__)
 
+DTYPE_TO_TAG = {
+    torch.bfloat16: 0,
+    torch.float16: 1,
+    torch.float32: 2,
+    torch.float64: 3,
+}
+
+TAG_TO_DTYPE = {v: k for k, v in DTYPE_TO_TAG.items()}
+
 class FastSerializer(Serializer):
     def __init__(self):
         super().__init__()
 
     def to_bytes(self, t: torch.Tensor) -> bytes:
-        # FIXME: only support fp16 for now
-        assert t.dtype == torch.float16
-        return t.contiguous().cpu().numpy().tobytes()
+        # make dtype into bit stream
+        tag = DTYPE_TO_TAG[t.dtype]
+        # make tensor into bit stream
+        buf = t.contiguous().cpu().view(torch.uint8).numpy().tobytes()
+        return tag.to_bytes(1, byteorder='big') + buf
 
 class FastDeserializer(Deserializer):
     def __init__(self):
         super().__init__()
 
     def from_bytes_normal(self, b: bytes) -> torch.Tensor:
-        return torch.frombuffer(b, dtype=torch.float16)
+        tag = int.from_bytes(buf[:1], byteorder='big')
+        buffer = b[:-1] # make it l-val
+        return torch.frombuffer(buffer, dtype=TAG_TO_DTYPE[tag])
 
     def from_bytes(self, b: bytes) -> torch.Tensor:
         return self.from_bytes_normal(b)
