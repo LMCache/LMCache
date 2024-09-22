46bdb8f1b5ae28cd2ebe2671b1f077ae2d113bf5
Xueshen Liu <liuxs@umich.edu>
2024-09-21 22:18:10 -0400

    Update fast_serde.py
    Adding `dtype` arg. Use meta data to determine data type.

--- lmcache/storage_backend/serde/fast_serde.py
+++ lmcache/storage_backend/serde/fast_serde.py
@@ -9,34 +9,21 @@ from lmcache.config import GlobalConfig
 
 logger = init_logger(__name__)
 
-DTYPE_TO_TAG = {
-    torch.bfloat16: 0,
-    torch.float16: 1,
-    torch.float32: 2,
-    torch.float64: 3,
-}
-
-TAG_TO_DTYPE = {v: k for k, v in DTYPE_TO_TAG.items()}
-
 class FastSerializer(Serializer):
     def __init__(self):
         super().__init__()
 
     def to_bytes(self, t: torch.Tensor) -> bytes:
-        # make dtype into bit stream
-        tag = DTYPE_TO_TAG[t.dtype]
         # make tensor into bit stream
         buf = t.contiguous().cpu().view(torch.uint8).numpy().tobytes()
-        return tag.to_bytes(1, byteorder='big') + buf
+        return buf
 
 class FastDeserializer(Deserializer):
     def __init__(self):
         super().__init__()
 
-    def from_bytes_normal(self, b: bytes) -> torch.Tensor:
-        tag = int.from_bytes(b[:1], byteorder='big')
-        buffer = b[1:] # make it l-val
-        return torch.frombuffer(buffer, dtype=TAG_TO_DTYPE[tag])
+    def from_bytes_normal(self, b: bytes, dtype=torch.bfloat16) -> torch.Tensor:
+        return torch.frombuffer(buffer, dtype=dtype)
 
     def from_bytes(self, b: bytes) -> torch.Tensor:
         return self.from_bytes_normal(b)
