4f3ef39956c3e92505de407b9d60e2c0f4cb6346
Sixian Xiong <xiongsx07@gmail.com>
2024-09-27 21:14:15 +0800

    Add offline inference example.

---
+++ examples/offline_inference/README.md
@@ -0,0 +1,5 @@
+# Feed offline prompts to the model
+1.  ```lmcache_server localhost 65432```  
+And wait until it's ready.  
+2. ```python3 offline_inference.py```  
+You can also modify the model and prompts inside this file.  
---
+++ examples/offline_inference/offline_inference.py
@@ -0,0 +1,28 @@
+import lmcache_vllm
+# Note that muct import lmcache_vllm first to enable LMCache.
+from vllm import LLM, SamplingParams
+import time
+
+# Sample prompts.
+prompts = [
+    "Hello, my name is",
+    "The most popular sports in the world is",
+    "The father of computer is",
+    "The future of AI is",
+]
+# Create a sampling params object.
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+
+# Create an LLM.
+llm = LLM(model="mistralai/Mistral-7B-Instruct-v0.2", gpu_memory_utilization= 0.8)
+# Generate texts from the prompts. The output is a list of RequestOutput objects
+# that contain the prompt, generated text, and other information.
+t1 = time.perf_counter()
+outputs = llm.generate(prompts, sampling_params)
+t2 = time.perf_counter()
+# Print the outputs.
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+print(f"Total Time: {t2 - t1} seconds")
