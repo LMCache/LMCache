b1b5502afaaa49530a60966d8a86dc9337a80263
ApostaC <jc4xvyp@outlook.com>
2024-10-22 15:00:58 -0500

    [Add] basic example for cacheblend

---
+++ examples/blend_kv/blend_kv.py
@@ -0,0 +1,46 @@
+from lmcache_vllm.vllm import LLM, SamplingParams
+from lmcache_vllm.blend_adapter import combine_input_prompt_chunks, append_separator
+import time
+import torch
+torch.multiprocessing.set_start_method('spawn')
+
+
+def precompute_kv(text_chunk, llm):
+    sampling_params_prefix = SamplingParams(temperature=0.0, top_p=0.95, max_tokens=1)
+    text_chunk = append_separator(text_chunk)
+    llm.generate([text_chunk], sampling_params_prefix)
+
+
+context_files = ["chunk1.txt", "chunk2.txt"]
+chunks = []
+
+for context_file in context_files:
+    with open(context_file, "r") as fin:
+        context = fin.read()
+    chunks.append(context)
+
+
+sys_prompt = "Here's a document from the user: "
+question = "Question: What does this document mainly talks about? Answer: "
+
+
+llm = LLM(model="mistralai/Mistral-7B-Instruct-v0.2", gpu_memory_utilization=0.5, tensor_parallel_size=1)
+sampling_params_generation = SamplingParams(temperature=0.0, top_p=0.95, max_tokens=30)
+
+
+print("-------------------- Pre-computing KV cache for the chunks -------------------------")
+for chunk in chunks:
+    precompute_kv(chunk, llm)
+
+print("Running the real query here!")
+
+user_prompt= [sys_prompt, chunks[0], chunks[1], question]
+user_prompt = combine_input_prompt_chunks(user_prompt)
+outputs = llm.generate(user_prompt, sampling_params_generation)
+for output in outputs:
+    generated_text = output.outputs[0].text
+    print(f"Newly generated text: {generated_text!r}")
+
+import lmcache_vllm
+lmcache_vllm.close_lmcache_engine()
+
