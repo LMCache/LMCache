36456f7b2cf5f26f287b5163daabd71acd2fbb05
Yuwei An <yuweia@andrew.cmu.edu>
2024-10-23 17:37:01 -0400

    default config

--- lmcache/storage_backend/serde/cachegen_basics.py
+++ lmcache/storage_backend/serde/cachegen_basics.py
@@ -7,6 +7,7 @@ import torch
 
 from lmcache.logging import init_logger
 from lmcache.utils import _lmcache_nvtx_annotate
+from transformers import AutoConfig
 
 logger = init_logger(__name__)
 
@@ -46,8 +47,7 @@ class CacheGenConfig:
                 nlayers=32,
                 kspecs=[
                     QuantizationSpec(start_layer=0, end_layer=10, bins=32),
-                    QuantizationSpec(start_layer=10, end_layer=20, bins=16),
-                    QuantizationSpec(start_layer=20, end_layer=32, bins=16),
+                    QuantizationSpec(start_layer=10, end_layer=32, bins=16),
                 ],
                 vspecs=[
                     QuantizationSpec(start_layer=0, end_layer=2, bins=32),
@@ -59,8 +59,7 @@ class CacheGenConfig:
                 nlayers=32,
                 kspecs=[
                     QuantizationSpec(start_layer=0, end_layer=10, bins=32),
-                    QuantizationSpec(start_layer=10, end_layer=20, bins=16),
-                    QuantizationSpec(start_layer=20, end_layer=32, bins=16),
+                    QuantizationSpec(start_layer=10, end_layer=32, bins=16),
                 ],
                 vspecs=[
                     QuantizationSpec(start_layer=0, end_layer=2, bins=32),
@@ -73,8 +72,7 @@ class CacheGenConfig:
                 nlayers=40,
                 kspecs=[
                     QuantizationSpec(start_layer=0, end_layer=10, bins=32),
-                    QuantizationSpec(start_layer=10, end_layer=20, bins=16),
-                    QuantizationSpec(start_layer=20, end_layer=40, bins=16),
+                    QuantizationSpec(start_layer=10, end_layer=40, bins=16),
                 ],
                 vspecs=[
                     QuantizationSpec(start_layer=0, end_layer=2, bins=32),
@@ -82,7 +80,44 @@ class CacheGenConfig:
                 ],
             )
         else:
-            raise ValueError(f"Model {model_name} is not supported")
+            try:
+                config = AutoConfig.from_pretrained(model_name)
+                # Default name caught by num_hidden_layers
+                if config.num_hidden_layers is None:
+                    raise ValueError(
+                        f"num_hidden_layers is None for model {model_name}")
+                if config.num_hidden_layers < 10: 
+                    return CacheGenConfig(
+                        nlayers=config.num_hidden_layers,
+                        kspecs=[
+                            QuantizationSpec(start_layer=0,
+                                            end_layer=config.num_hidden_layers,
+                                            bins=32),
+                        ],
+                        vspecs=[
+                            QuantizationSpec(start_layer=0,
+                                            end_layer=config.num_hidden_layers,
+                                            bins=32),
+                        ],
+                    )
+                else:
+                    return CacheGenConfig(
+                        nlayers=config.num_hidden_layers,
+                        kspecs=[
+                            QuantizationSpec(start_layer=0, end_layer=10, bins=32),
+                            QuantizationSpec(start_layer=10,
+                                            end_layer=config.num_hidden_layers,
+                                            bins=16),
+                        ],
+                        vspecs=[
+                            QuantizationSpec(start_layer=0, end_layer=2, bins=32),
+                            QuantizationSpec(start_layer=2,
+                                            end_layer=config.num_hidden_layers,
+                                            bins=16),
+                        ],
+            except Exception as e:
+                logger.error(f"Failed to get CacheGenConfig: {e}")
+                return None
 
 
 @dataclass
