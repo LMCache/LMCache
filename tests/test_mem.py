d74ae08bbff21223d4a5abeee4923bc5c83460df
YaoJiayi <120040070@link.cuhk.edu.cn>
2024-10-24 14:02:02 -0500

    add test

---
+++ tests/test_mem.py
@@ -0,0 +1,35 @@
+import torch
+import pytest
+import lmc_ops
+
+# TODO(Jiayi): add more dtypes
+def test_fast_mem_load():
+    blk_size = 16
+    num_blk = 1000
+    num_tok = num_blk * blk_size
+    num_load = 100
+    kv_dtype = torch.bfloat16
+    # [num_blk, blk_size, num_head, head_size]
+    kv_shape = [num_blk, blk_size, 8, 128]
+    slot_mapping = random.sample(range(0, num_tok+1), num_load)
+    kv_cache = [torch.rand(kv_shape, dtype=kv_dtype, device='cuda'),
+                torch.rand(kv_shape, dtype=kv_dtype, device='cuda')]
+    
+    key_cuda, value_cuda = lmc_ops.load_and_reshape_flash(
+        kv_cache[0],
+        kv_cache[1],
+        slot_mapping,
+        "auto",
+        1.0,
+        1.0
+    )
+    
+    key_cache = kv_caches[0].reshape(-1, num_heads, head_size)
+    value_cache = kv_caches[1].reshape(-1, num_heads, head_size)
+    key = key_cache[slot_mapping]
+    value = value_cache[slot_mapping]
+    
+    assert torch.allclose(key, key_cuda, atol=1e-5)
+    assert torch.allclose(value, value_cuda, atol=1e-5)
+    
+    
