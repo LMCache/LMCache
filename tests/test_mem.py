968929ba22678ca564012dc19f55ebc5cf77da9b
YaoJiayi <120040070@link.cuhk.edu.cn>
2024-10-24 14:05:20 -0500

    fix test

--- tests/test_mem.py
+++ tests/test_mem.py
@@ -8,9 +8,11 @@ def test_fast_mem_load():
     num_blk = 1000
     num_tok = num_blk * blk_size
     num_load = 100
+    num_head = 8
+    head_size = 128
     kv_dtype = torch.bfloat16
     # [num_blk, blk_size, num_head, head_size]
-    kv_shape = [num_blk, blk_size, 8, 128]
+    kv_shape = [num_blk, blk_size, num_head, head_size]
     slot_mapping = random.sample(range(0, num_tok+1), num_load)
     kv_cache = [torch.rand(kv_shape, dtype=kv_dtype, device='cuda'),
                 torch.rand(kv_shape, dtype=kv_dtype, device='cuda')]
@@ -24,8 +26,8 @@ def test_fast_mem_load():
         1.0
     )
     
-    key_cache = kv_caches[0].reshape(-1, num_heads, head_size)
-    value_cache = kv_caches[1].reshape(-1, num_heads, head_size)
+    key_cache = kv_caches[0].reshape(-1, num_head, head_size)
+    value_cache = kv_caches[1].reshape(-1, num_head, head_size)
     key = key_cache[slot_mapping]
     value = value_cache[slot_mapping]
     
