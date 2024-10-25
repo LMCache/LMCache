1f4ef0e8ccb59f9617f6325454e99d5498b30686
Sixian Xiong <xiongsx07@gmail.com>
2024-10-25 09:55:50 +0800

    Fix docs

--- lmcache/cache_engine.py
+++ lmcache/cache_engine.py
@@ -185,13 +185,13 @@ class LMCacheEngine:
         tokens: torch.Tensor,
         kv_tensors: torch.Tensor,
         fmt: str,
-        num_skip_chunk=0,
+        num_skip_prefix_chunk=0,
     ) -> Iterable[Tuple[str, torch.Tensor]]:
         """
         Skip the existing chunks and return the rest of the chunks
         """
         chunk_hashes = self._prefix_hash(self._chunk_tokens(tokens),
-                                         num_skip_chunk)
+                                         num_skip_prefix_chunk)
         # With num_skip_chunks, the following is relative to
         # the new start after skip.
         num_tokens: int = self._num_tokens_in_kv(kv_tensors, fmt)
@@ -216,7 +216,7 @@ class LMCacheEngine:
         tokens: torch.Tensor,
         kv_tensors: torch.Tensor,
         fmt: str,
-        num_skip_chunk=0,
+        num_skip_prefix_chunk=0,
         skip_existing=True,
     ) -> Iterable[Tuple[str, torch.Tensor]]:
         """
@@ -224,9 +224,9 @@ class LMCacheEngine:
         """
         if skip_existing:
             return self._make_chunks_skip_existing(tokens, kv_tensors, fmt,
-                                                   num_skip_chunk)
+                                                   num_skip_prefix_chunk)
         else:
-            assert num_skip_chunk == 0
+            assert num_skip_prefix_chunk == 0
             return zip(
                 self._prefix_hash(self._chunk_tokens(tokens)),
                 self._chunk_kv(kv_tensors, fmt),
@@ -250,7 +250,9 @@ class LMCacheEngine:
             kv_tensors_raw: the kv cache of the tokens, in the format of nested 
             tuples
             kv_tensors_mask: a boolean mask of tokens indicating which tokens'
-            KV Cache should be stored. Currently, only support suffix mask.
+            KV Cache should be stored. Only support suffix mask.
+            len(kv_tensors_mask) should be the same as len(tokens)
+            number of true should be the same as kv_tensors_raw token number
             
             format: either 'huggingface' or 'vllm'
                 For huggingface, it should have the shape of 
@@ -276,16 +278,11 @@ class LMCacheEngine:
         f"Invalid shape of mask: {kv_tensors_mask.shape}"
         assert len(tokens) == len(kv_tensors_mask), \
             "token length does not match mask length"
-        num_skip_chunk = 0
-        num_skip_tok = 0
-        if kv_tensors_mask is not None:
-            # NOTE(Sixian): Now kv_tensors_mask always a suffix mask.
-            num_skip_tok = (len(kv_tensors_mask) - torch.sum(kv_tensors_mask))
-            num_skip_chunk = num_skip_tok // self.chunk_size
+        # NOTE(Sixian): Now kv_tensors_mask always a suffix mask.
+        num_skip_tok = (len(kv_tensors_mask) - torch.sum(kv_tensors_mask))
+        num_skip_chunk = num_skip_tok // self.chunk_size
         assert num_skip_tok == num_skip_chunk * self.chunk_size, \
-            "Only support chunk skip in store."
-        assert num_skip_tok < len(tokens), "No tokens to store"
-        assert len(kv_tensors_raw) > 0, "Empty kv_tensors"
+            "Store KV mask should align to chunk size"
         assert len(tokens) == self._num_tokens_in_kv(
             kv_tensors_raw, fmt
         ) + num_skip_tok, \
@@ -425,7 +422,7 @@ class LMCacheEngine:
 
         Output:
             a boolean tensor indicating the existence of the KV cache of 
-            each token
+            each token. The returned tensor only has true in prefix.
         """
         # NOTE(Sixian): Now this is a prefix lookup.
         fmt = self.metadata.fmt
