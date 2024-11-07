fd217d02aca51c1995ad601b8a83ac0254ff7845
YaoJiayi <120040070@link.cuhk.edu.cn>
2024-11-07 03:22:56 -0600

    fix format

--- lmcache/storage_backend/mem_pool/cpu_pool.py
+++ lmcache/storage_backend/mem_pool/cpu_pool.py
@@ -1,33 +1,38 @@
 from typing import Optional
+
 import torch
 
 from lmcache.config import LMCacheEngineConfig
-from lmcache.storage_backend.mem_pool.base_pool import BasePool, KVObj
 from lmcache.logging import init_logger
+from lmcache.storage_backend.mem_pool.base_pool import BasePool, KVObj
 
 logger = init_logger(__name__)
 
+
 class LocalCPUPool(BasePool):
-    def __init__(self, config: LMCacheEngineConfig):  
+
+    def __init__(self, config: LMCacheEngineConfig):
         self.chunk_size = config.chunk_size
         # TODO(Jiayi): the `max_chunk_num` should be computed
-        # from `config.max_cache_size`   
+        # from `config.max_cache_size`
         max_chunk_num = 200
         use_pinned_memory = True
-        max_num_tok = max_chunk_num * self.chunk_size
         kv_dtype = torch.bfloat16
-        
-        #mem_shape = (32, 2, max_num_tok, 8, 128)
+
         mem_shape = (32, 2, self.chunk_size, 8, 128)
-        logger.info(f"Initializing cpu mem buffer, is_pinned: {use_pinned_memory}")
+        logger.info(
+            f"Initializing cpu mem buffer, is_pinned: {use_pinned_memory}")
         with torch.inference_mode():
-            #self.mem_pool = torch.empty(mem_shape, dtype=kv_dtype,
-            #    device='cpu', pin_memory=use_pinned_memory)
-            self.mem_pool = [torch.empty(mem_shape, dtype=kv_dtype,
-                device='cpu', pin_memory=use_pinned_memory) for i in range(max_chunk_num)]
-        
+            self.mem_pool = [
+                torch.empty(mem_shape,
+                            dtype=kv_dtype,
+                            device='cpu',
+                            pin_memory=use_pinned_memory)
+                for i in range(max_chunk_num)
+            ]
+
         self.free_pool = [i for i in range(max_chunk_num)]
-            
+
     def allocate(self, kv_chunk: torch.Tensor) -> Optional[KVObj]:
         """
         Allocate a buffer memory pointer from the memory pool.
@@ -48,8 +53,7 @@ class LocalCPUPool(BasePool):
             logger.warning("No free memory chunks. Evictor might be failing!")
         chunk_idx = self.free_pool.pop()
         return KVObj(chunk_idx, self.mem_pool[chunk_idx][:, :, 0:num_tok])
-        
-        
+
     def free(self, kv_obj: KVObj):
         """
         Free the corresponding memory chunk
