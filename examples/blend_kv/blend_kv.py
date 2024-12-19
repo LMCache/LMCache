ccca24996d54312834a23266429d8fa8f05a14ab
Sixian Xiong <xiongsx07@gmail.com>
2024-12-19 16:20:53 +0800

    Update precompute.

--- examples/blend_kv/blend_kv.py
+++ examples/blend_kv/blend_kv.py
@@ -1,18 +1,9 @@
 import lmcache_vllm
 import torch
-from lmcache_vllm.blend_adapter import (append_separator,
-                                        combine_input_prompt_chunks)
+from lmcache_vllm.blend_adapter import combine_input_prompt_chunks, OfflineKVPreCompute
 from lmcache_vllm.vllm import LLM, SamplingParams
 
 torch.multiprocessing.set_start_method('spawn')
-
-
-def precompute_kv(text_chunk, llm):
-    sampling_params_prefix = SamplingParams(temperature=0.0,
-                                            top_p=0.95,
-                                            max_tokens=1)
-    text_chunk = append_separator(text_chunk)
-    llm.generate([text_chunk], sampling_params_prefix)
 
 
 context_files = ["chunk1.txt", "chunk2.txt"]
@@ -27,7 +18,7 @@ sys_prompt = "Here's a document from the user: "
 question = "Question: What does this document mainly talks about? Answer: "
 
 llm = LLM(model="mistralai/Mistral-7B-Instruct-v0.2",
-          gpu_memory_utilization=0.5,
+          gpu_memory_utilization=0.7,
           tensor_parallel_size=1)
 sampling_params_generation = SamplingParams(temperature=0.0,
                                             top_p=0.95,
@@ -35,8 +26,9 @@ sampling_params_generation = SamplingParams(temperature=0.0,
 
 print(
     "-------------- Pre-computing KV cache for the chunks -------------------")
+offline_precompute = OfflineKVPreCompute(llm)
 for chunk in chunks:
-    precompute_kv(chunk, llm)
+    offline_precompute.precompute_kv(chunk)
 
 print("Running the real query here!")
 
@@ -46,6 +38,8 @@ outputs = llm.generate(user_prompt, sampling_params_generation)
 for output in outputs:
     generated_text = output.outputs[0].text
     print(f"Newly generated text: {generated_text!r}")
+    ttft = output.metrics.first_token_time - output.metrics.first_scheduled_time
+    print(f"Time to first token: {ttft:.3f} seconds")
 
 # Graceful exit
 lmcache_vllm.close_lmcache_engine()
