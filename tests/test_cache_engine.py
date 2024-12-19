d6dae25c4c45ba9b0677c85d03e5fbf3d2834706
Sixian Xiong <xiongsx07@gmail.com>
2024-12-20 00:15:54 +0800

    Fix insintance subclass problem in test_env_configure

--- tests/test_cache_engine.py
+++ tests/test_cache_engine.py
@@ -444,11 +444,11 @@ def test_env_configure():
     newconfig = LMCacheEngineConfig.from_env()
     for attr in fields(newconfig):
         value = getattr(newconfig, attr.name)
-        if isinstance(value, int) or isinstance(value, float):
+        if type(value) == int or type(value) == float:
             assert value == 0
-        elif isinstance(value, bool):
+        elif type(value) == bool:
             assert value is bool("0")
-        elif isinstance(value, str):
+        elif type(value) == str:
             assert value == "0"
         else:
             raise AssertionError("Unexpected type in LMCacheEngineConfig")
