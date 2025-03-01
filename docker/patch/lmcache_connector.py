f428f398e7deff16e62fcff2e7d248ed4cb09259
baoloongmao <baoloongmao@tencent.com>
2025-03-01 11:56:20 +0800

    Update Dockerfile to align vllm v0.7.3

--- docker/patch/lmcache_connector.py
+++ docker/patch/lmcache_connector.py
@@ -1,14 +1,16 @@
+# SPDX-License-Identifier: Apache-2.0
 """
-Simple KV Cache Connector for Distributed Machine Learning Inference
+LMCache KV Cache Connector for Distributed Machine Learning Inference
 
 The LMCacheConnector can (1) transfer KV caches between prefill vLLM worker
 (KV cache producer) and decode vLLM worker (KV cache consumer) using LMCache;
-(2) offload and share KV caches. Only (2) is supported for now.
+(2) offload and share KV caches.
 """
 
 from typing import TYPE_CHECKING, List, Tuple, Union
 
 import torch
+
 from vllm.config import VllmConfig
 from vllm.distributed.kv_transfer.kv_connector.base import KVConnectorBase
 from vllm.logger import init_logger
@@ -32,10 +34,11 @@ class LMCacheConnector(KVConnectorBase):
         self.transfer_config = config.kv_transfer_config
         self.vllm_config = config
 
+        from lmcache.experimental.cache_engine import LMCacheEngineBuilder
+        from lmcache.integration.vllm.utils import ENGINE_NAME
         from lmcache.integration.vllm.vllm_adapter import (
             RetrieveStatus, StoreStatus, init_lmcache_engine,
             lmcache_retrieve_kv, lmcache_should_store, lmcache_store_kv)
-
         logger.info("Initializing LMCacheConfig under kv_transfer_config %s",
                     self.transfer_config)
 
@@ -43,6 +46,8 @@ class LMCacheConnector(KVConnectorBase):
         self.engine = init_lmcache_engine(config.model_config,
                                           config.parallel_config,
                                           config.cache_config)
+        self.lmcache_engine_name = ENGINE_NAME
+        self.lmcache_engine_builder = LMCacheEngineBuilder
 
         self.model_config = config.model_config
         self.parallel_config = config.parallel_config
@@ -60,11 +65,10 @@ class LMCacheConnector(KVConnectorBase):
     ) -> Tuple[Union[torch.Tensor, IntermediateTensors], bool,
                "ModelInputForGPUWithSamplingMetadata"]:
 
-        # TODO(Jiayi): This shouldn't be none for disagg prefill
         hidden_or_intermediate_states = None
 
-        # TODO (Jiayi): Only normal prefill is supported for now
-        retrieve_status = [self.retrieve_status.PREFILL]
+        # TODO (Jiayi): Need to support chunked prefill
+        retrieve_status = self.retrieve_status.PREFILL
 
         model_input, bypass_model_exec = self.lmcache_retrieve_kv(
             model_executable, model_input, self.cache_config, kv_caches,
@@ -80,8 +84,15 @@ class LMCacheConnector(KVConnectorBase):
         hidden_or_intermediate_states: Union[torch.Tensor,
                                              IntermediateTensors],
     ) -> None:
+        num_reqs = 0
+        seq_group_list = model_input.sampling_metadata.seq_groups
+        assert seq_group_list is not None
+        for seq_group in seq_group_list:
+            seq_ids = seq_group.seq_ids
+            for seq_id in seq_ids:
+                num_reqs += 1
+
         # TODO (Jiayi): Only normal prefill is supported for now
-        #store_status = [self.store_status.PREFILL] * num_reqs
         store_status = self.lmcache_should_store(model_input)
         self.lmcache_store_kv(
             self.model_config,
@@ -94,4 +105,4 @@ class LMCacheConnector(KVConnectorBase):
         )
 
     def close(self):
-        self.engine.close()
+        self.lmcache_engine_builder.destroy(self.lmcache_engine_name)
