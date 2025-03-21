7af11e935b0114ecb3672a57237f0a81ac28680d
Siddhant Ray <siddhant.r98@gmail.com>
2025-03-20 23:16:30 -0500

    format fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,7 @@ extensions = [
 copybutton_prompt_text = r"^(\$ |>>> |\# )"
 copybutton_prompt_is_regexp = True
 
+
 class MockedClassDocumenter(autodoc.ClassDocumenter):
     """Remove note about base class when a class is 
     derived from object."""
