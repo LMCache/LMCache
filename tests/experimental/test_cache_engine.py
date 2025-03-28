f6d3d7d68e538e64ffa7e69dc4dd9feb9a01ee9f
YaoJiayi <120040070@link.cuhk.edu.cn>
2025-03-28 05:21:17 +0000

    format fix

--- tests/experimental/test_cache_engine.py
+++ tests/experimental/test_cache_engine.py
@@ -68,7 +68,7 @@ def test_same_retrieve_store(autorelease_experimental):
 
 @pytest.mark.parametrize("fmt", ["vllm"])
 @pytest.mark.parametrize("chunk_size", [128, 256])
-@pytest.mark.parametrize("backend", 
+@pytest.mark.parametrize("backend",
                          ["cpu", "local_disk", "remote", "remote_cachegen"])
 @pytest.mark.parametrize("lmserver_experimental_process", ["cpu"],
                          indirect=True)
