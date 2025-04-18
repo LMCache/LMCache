437364cdf9fb15ccf4204aefe1c3c05a3f0eebe6
ApostaC <yihua98@uchicago.edu>
2025-04-17 19:09:54 -0700

    fix format again

--- tests/disagg/test_nixl_channel_v2.py
+++ tests/disagg/test_nixl_channel_v2.py
@@ -163,7 +163,7 @@ def send_and_measure_throughput_v2(channel: NixlChannel,
                         f" in {this_round:.6f} seconds")
             if simulate_workload:
                 time.sleep(0.05)  # Sleep 50ms between batches
-        elapsed_time = sum(elapsed_times) # type: ignore
+        elapsed_time = sum(elapsed_times)  # type: ignore
         logger.info(f"Elapsed times: {elapsed_times}")
 
     logger.info(f"Sent {len(objs)} objects in {elapsed_time:.6f} seconds")
