cabb82806323c8c8effc9af769dc626ca95572e3
YaoJiayi <120040070@link.cuhk.edu.cn>
2025-04-18 20:05:49 +0000

    fix format

--- lmcache/experimental/cache_controller/controllers/__init__.py
+++ lmcache/experimental/cache_controller/controllers/__init__.py
@@ -1,7 +1,7 @@
 from lmcache.experimental.cache_controller.controllers.kv_controller import \
     KVController
 from lmcache.experimental.cache_controller.controllers.registration_controller import \
-    RegistrationController
+    RegistrationController  # noqa: E501
 
 __all__ = [
     "KVController",
