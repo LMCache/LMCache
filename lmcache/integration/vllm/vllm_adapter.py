edc6f71bcbbf9f6793751b5d0353f5a70056421b
YaoJiayi <120040070@link.cuhk.edu.cn>
2025-04-20 17:51:35 +0000

    use gpu buffer to store

--- lmcache/integration/vllm/vllm_adapter.py
+++ lmcache/integration/vllm/vllm_adapter.py
@@ -85,13 +85,18 @@ def init_lmcache_engine(
     kv_shape = (num_layer, 2, chunk_size, num_kv_head, head_size)
 
     # Change current device.
-    torch.cuda.device(parallel_config.rank)
+    device = torch.cuda.device(parallel_config.rank)
     metadata = LMCacheEngineMetadata(model_config.model,
                                      parallel_config.world_size,
                                      parallel_config.rank, "vllm", kv_dtype,
                                      kv_shape)
     hidden_dim_size = num_kv_head * head_size
-    vllm_gpu_connector = VLLMPagedMemGPUConnectorV2(hidden_dim_size, num_layer)
+    vllm_gpu_connector = VLLMPagedMemGPUConnectorV2(
+        hidden_dim_size, num_layer,
+        use_gpu=True,
+        chunk_size=chunk_size,
+        dtype=kv_dtype,
+        device=device)
     assert isinstance(config, LMCacheEngineConfig), \
         "LMCache experimental configuration is should be passed."
     engine = LMCacheEngineBuilder.get_or_create(ENGINE_NAME, config, metadata,
