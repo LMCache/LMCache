ae36c124b0d9b7693286b8fbae96adc174315c34
YaoJiayi <120040070@link.cuhk.edu.cn>
2025-04-28 02:49:45 +0000

    add pos encoding kernel

--- csrc/pybind.cpp
+++ csrc/pybind.cpp
@@ -17,6 +17,7 @@
 #include <pybind11/pybind11.h>
 #include "mem_kernels.cuh"
 #include "cachegen_kernels.cuh"
+#include "pos_kernels.cuh"
 #include <torch/torch.h>
 #include <iostream>
 
@@ -30,4 +31,5 @@ PYBIND11_MODULE(c_ops, m) {
     m.def("decode_fast_new", &decode_cuda_new);
     m.def("decode_fast_prefsum", &decode_cuda_prefsum);
     m.def("calculate_cdf", &calculate_cdf);
+    m.def("rotary_embedding_k_fused", &rotary_embedding_k_fused);
 }
