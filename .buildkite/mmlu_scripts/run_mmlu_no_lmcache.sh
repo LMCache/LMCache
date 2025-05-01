fb268d9781e6176ccff2a07b55cd26e5580b6787
Samuel Shen <slshen@uchicago.edu>
2025-04-30 22:42:31 -0500

    changes

--- .buildkite/mmlu_scripts/run_mmlu_no_lmcache.sh
+++ .buildkite/mmlu_scripts/run_mmlu_no_lmcache.sh
@@ -22,10 +22,8 @@ python3 -m vllm.entrypoints.api_server \
 SERVER_PID=$!
 sleep 15
 
-python3 ./buildkite/scripts/mmlu_bench.py \
-  --backend vllm \
-  --host http://localhost \
-  --port $PORT \
+python3 ./buildkite/mmlu_scripts/mmlu_bench.py \
+  --nsub 60 \
   --parallel 16 \
   > mmlu-results/v0_deepseek2.txt || true
 
