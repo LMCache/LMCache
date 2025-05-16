7e07b0193bf9b03200f165e106ed70dd1051b268
Martin Hickey <martin.hickey@ie.ibm.com>
2025-05-16 16:33:52 +0100

    Update docker specification to be compatible with latest vLLM OpenAI server
    Updated the docker specification as follows:
    
    - Removed redundant commands
    - Updated requirements to latest packages
    - Add build args to make build configurable for different vLLM images
    - Added example build script
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

--- docker/example_run.sh
+++ docker/example_run.sh
@@ -1,6 +1,16 @@
-IMAGE=<IMAGE_NAME>:<TAG>
-docker run --runtime nvidia --gpus all \
-    --env "HF_TOKEN=<YOUR_HUGGINGFACE_TOKEN>" \
+# Example script to run the container vLLM OpenAI server with LMCache
+#
+# Prerequisite:
+# - If CUDA then require NVIDIA Container Toolkit:
+# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+
+# Set the following variables:
+IMAGE=
+HF_MODEL_NAME='meta-llama/Llama-3.1-8B-Instruct'
+RUNTIME=nvidia
+
+docker run --runtime=$RUNTIME --gpus all \
+    --env "HF_TOKEN=<HF_TOKEN_ACCESS_MODEL>" \
     --env "LMCACHE_USE_EXPERIMENTAL=True" \
     --env "chunk_size=256" \
     --env "local_cpu=True" \
@@ -9,6 +19,5 @@ docker run --runtime nvidia --gpus all \
     --network host \
     --entrypoint "/usr/local/bin/vllm" \
     $IMAGE \
-    serve mistralai/Mistral-7B-Instruct-v0.2 --kv-transfer-config \
-    '{"kv_connector":"LMCacheConnector","kv_role":"kv_both"}' \
-    --enable-chunked-prefill false
+    serve $HF_MODEL_NAME --kv-transfer-config \
+    '{"kv_connector":"LMCacheConnectorV1","kv_role":"kv_both"}'
