d84e2236ee0023d3e707640933214640a788f86d
Martin Hickey <martin.hickey@ie.ibm.com>
2025-05-19 18:28:27 +0100

    [CI/Build]: Update docker specification to be compatible with latest vLLM OpenAI server (#665)
    * Update docker specification to be compatible with latest vLLM OpenAI server
    
    Updated the docker specification as follows:
    
    - Removed redundant commands
    - Updated requirements to latest packages
    - Add build args to make build configurable for different vLLM images
    - Added example build script
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    
    * Update after review
    
    Review comments:
    
    - https://github.com/LMCache/LMCache/pull/665#discussion_r2094668070
    - https://github.com/LMCache/LMCache/pull/665#discussion_r2094698460
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    
    ---------
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>
    Signed-off-by: zhuohangu <zhuohan@uchicago.edu>

---
+++ docker/example_build.sh
@@ -0,0 +1,19 @@
+# Example script to build the LMCache container image
+
+# Update the following variables accordingly
+# Note: latest (built from GitHub) images of vLLM are in AWS ECR registry:
+# public.ecr.aws/q9t5s3a7/vllm-ci-test-repo
+# The latest release version of vLLM imnage are on DockerHub: vllm/vllm-openai
+CUDA_VERSION=12.8.1
+DOCKERFILE_NAME='Dockerfile'
+DOCKER_BUILD_PATH='.'
+UBUNTU_VERSION=22.04
+VLLM_IMAGE_REPO='vllm/vllm-openai'
+VLLM_TAG='latest'
+
+docker build \
+    --build-arg CUDA_VERSION=$CUDA_VERSION \
+    --build-arg UBUNTU_VERSION=$UBUNTU_VERSION \
+    --build-arg VLLM_IMAGE_REPO=$VLLM_IMAGE_REPO \
+    --build-arg VLLM_TAG=$VLLM_TAG \
+    -f $DOCKERFILE_NAME $DOCKER_BUILD_PATH
--- docker/example_run.sh
+++ docker/example_run.sh
@@ -1,14 +1,22 @@
-IMAGE=<IMAGE_NAME>:<TAG>
-docker run --runtime nvidia --gpus all \
-    --env "HF_TOKEN=<YOUR_HUGGINGFACE_TOKEN>" \
+# Example script to run the container vLLM OpenAI server with LMCache
+#
+# Prerequisite:
+# - If CUDA then require NVIDIA Container Toolkit:
+# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+
+# Set the following variables:
+IMAGE=
+HF_MODEL_NAME='meta-llama/Llama-3.1-8B-Instruct'
+RUNTIME=nvidia
+
+docker run --runtime=$RUNTIME --gpus all \
+    --env "HF_TOKEN=<HF_TOKEN_ACCESS_MODEL>" \
     --env "LMCACHE_USE_EXPERIMENTAL=True" \
     --env "chunk_size=256" \
     --env "local_cpu=True" \
     --env "max_local_cpu_size=5" \
-    -v ~/.cache/huggingface:/root/.cache/huggingface \
+    --volume ~/.cache/huggingface:/root/.cache/huggingface \
     --network host \
-    --entrypoint "/usr/local/bin/vllm" \
     $IMAGE \
-    serve mistralai/Mistral-7B-Instruct-v0.2 --kv-transfer-config \
-    '{"kv_connector":"LMCacheConnector","kv_role":"kv_both"}' \
-    --enable-chunked-prefill false
+    $HF_MODEL_NAME --kv-transfer-config \
+    '{"kv_connector":"LMCacheConnectorV1","kv_role":"kv_both"}'
