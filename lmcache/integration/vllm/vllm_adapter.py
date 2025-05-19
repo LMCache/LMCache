2a89a1a830f3a22e5ff8bbb4eb99e2b86715dc02
Shaoting <shaotingf@uchicago.edu>
2025-05-19 14:45:53 -0500

    trigger
    Signed-off-by: Shaoting <shaotingf@uchicago.edu>

--- lmcache/integration/vllm/vllm_adapter.py
+++ lmcache/integration/vllm/vllm_adapter.py
@@ -74,7 +74,7 @@ class RetrieveStatus(Enum):
     # (2) chunk_prefill_last
     # (3) prefix_prefill
     CHUNK_PREFILL = 2  # not last chunk
-    NONE = 4
+    NONE = 5
 
 
 def need_gpu_interm_buffer(lmcache_config: LMCacheEngineConfig):
