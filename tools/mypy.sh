3e9d3c3690801fd4b9e4e3b2da50c62dd9b25643
Martin Hickey <martin.hickey@ie.ibm.com>
2025-05-20 15:25:13 +0100

    Move linting to pre-commit
    Using pre-commit for linting, formatting and type checking.
    
    This replaces the format.sh bash script and tools dependencies
    in requirements-lint file to be managed by pre-commit ecosystem.
    It also provides checking at commit time.
    
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

---
+++ tools/mypy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CI=${1:-0}
+PYTHON_VERSION=${2:-local}
+
+if [ "$CI" -eq 1 ]; then
+    set -e
+fi
+
+if [ $PYTHON_VERSION == "local" ]; then
+    PYTHON_VERSION=$(python -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')
+fi
+
+run_mypy() {
+    echo "Running mypy on $1"
+    if [ "$CI" -eq 1 ] && [ -z "$1" ]; then
+        mypy --python-version "${PYTHON_VERSION}" "$@"
+        return
+    fi
+    mypy --follow-imports skip --python-version "${PYTHON_VERSION}" "$@"
+}
+
+run_mypy # Note that this is less strict than CI
+run_mypy tests
+run_mypy lmcache
