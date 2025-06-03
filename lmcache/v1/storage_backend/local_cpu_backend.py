11697b6f9a9d475daf262d97635fa518fe8289b3
杨现 <yangxianpku@pku.edu.cn>
2025-06-03 09:50:20 +0800

    [BUGFIX] CPU Memory Usage Update
    CPU Memory Usage Update

--- lmcache/v1/storage_backend/local_cpu_backend.py
+++ lmcache/v1/storage_backend/local_cpu_backend.py
@@ -104,6 +104,7 @@ class LocalCPUBackend(StorageBackendInterface):
         with self.cpu_lock:
             if key in self.hot_cache:
                 old_memory_obj = self.hot_cache.pop(key)
+                self.usage -= old_memory_obj.get_size()
                 old_memory_obj.ref_count_down()
             self.hot_cache[key] = memory_obj
             memory_obj.ref_count_up()
