0e0fb2f684f04755b47bd18b922d9a5f96a4303a
Serapheim Dimitropoulos <serapheim.dimitropoulos@weka.io>
2025-06-12 16:27:44 -0400

    [Fix] Fix race between test_gds/weka rmtree and asyncio thread loop
    Signed-off-by: Serapheim Dimitropoulos <serapheim.dimitropoulos@weka.io>

--- tests/v1/test_gds.py
+++ tests/v1/test_gds.py
@@ -107,9 +107,15 @@ def test_gds_backend_sanity():
         assert returned_memory_obj.get_shape() == memory_obj.get_shape()
         assert returned_memory_obj.get_dtype() == memory_obj.get_dtype()
     finally:
-        if os.path.exists(GDS_DIR):
-            shutil.rmtree(GDS_DIR)
         if thread_loop.is_running():
             thread_loop.call_soon_threadsafe(thread_loop.stop)
         if thread.is_alive():
             thread.join()
+        # We rmtree AFTER we ensure that the thread loop is done.
+        # This way we don't hit any race conditions in rmtree()
+        # where temp files are renamed while we try to unlink them.
+        # We also take care of any other errors with ignore_errors=True
+        # so if we want to run tests in parallel in the future they
+        # don't make each other fail.
+        if os.path.exists(GDS_DIR):
+            shutil.rmtree(GDS_DIR)
