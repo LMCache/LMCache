de9ef41c2ecdf45ccf3fbec7045325ba86bcabfa
calvin chen <120380290@qq.com>
2025-06-16 21:43:20 +0800

    add external connector adaptor
    Signed-off-by: calvin chen <120380290@qq.com>

--- lmcache/v1/storage_backend/connector/__init__.py
+++ lmcache/v1/storage_backend/connector/__init__.py
@@ -219,6 +219,7 @@ def CreateConnector(
     - blackhole://[any_text]
     - audit://host:port[?verify=true|false]
     - fs://[host:port]/path
+    - external://host:port/module_path/?connector_name=ConnectorName
 
     Examples:
     - redis://localhost:6379
@@ -230,6 +231,7 @@ def CreateConnector(
     - blackhole://
     - audit://localhost:8080?verify=true
     - fs:///tmp/lmcache
+    - external://host:0/adt.adt_kv_connector/?connector_name=AdtKVConnector
 
     Args:
         url: The remote URL
