df0dbc38b0a8999dd8e3cb41f29dea61e68ac198
Shaoting Feng <shaotingf@uchicago.edu>
2025-06-23 17:00:04 +0000

    Test packages in step 1
    Signed-off-by: Shaoting Feng <shaotingf@uchicago.edu>

--- .buildkite/install-env.sh
+++ .buildkite/install-env.sh
@@ -23,3 +23,6 @@ uv pip install -r requirements/test.txt
 export CUDA_HOME="/usr/local/cuda-${CUDA_VERSION}"
 export LD_LIBRARY_PATH="${CUDA_HOME}/lib64:${LD_LIBRARY_PATH:-}"
 export PATH="${CUDA_HOME}/bin:${PATH}"
+
+echo "📦 Installed packages in venv:"
+uv pip freeze
