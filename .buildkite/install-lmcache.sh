b96cd2672939f41e2b26af7af513b2ef101a6901
Shaoting Feng <shaotingf@uchicago.edu>
2025-07-02 21:41:54 +0000

    Update buildkite python version
    Signed-off-by: Shaoting Feng <shaotingf@uchicago.edu>

--- .buildkite/install-lmcache.sh
+++ .buildkite/install-lmcache.sh
@@ -3,11 +3,11 @@
 set -xe 
 
 VENV_DIR=".venv"
-PYTHON_BIN="/usr/bin/python3.10"
+PYTHON_BIN="/usr/bin/python3.11"
 if [[ -d "$VENV_DIR" ]]; then
   echo "⟳ Using existing venv: $(pwd)/$VENV_DIR"
 else
-  echo "⚙️  Creating venv with Python 3.10 at: $(pwd)/$VENV_DIR"
+  echo "⚙️  Creating venv with Python 3.11 at: $(pwd)/$VENV_DIR"
   # use uv for fast venv creation
   uv venv --python "$PYTHON_BIN" "$VENV_DIR"
 fi
