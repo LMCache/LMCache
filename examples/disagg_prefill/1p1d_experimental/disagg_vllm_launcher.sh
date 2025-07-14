c731f5f16db86b2d70aad1e76977f65f811dea97
Shaoting Feng <shaotingf@uchicago.edu>
2025-07-14 16:20:23 +0000

    Clean up
    Signed-off-by: Shaoting Feng <shaotingf@uchicago.edu>

--- examples/disagg_prefill/1p1d_experimental/disagg_vllm_launcher.sh
+++ examples/disagg_prefill/1p1d_experimental/disagg_vllm_launcher.sh
@@ -24,15 +24,14 @@ if [[ $1 == "prefiller" ]]; then
         LMCACHE_CONFIG_FILE=$prefill_config_file \
         VLLM_ENABLE_V1_MULTIPROCESSING=1 \
         VLLM_WORKER_MULTIPROC_METHOD=spawn \
-        CUDA_VISIBLE_DEVICES=4,1 \
+        CUDA_VISIBLE_DEVICES=0 \
         vllm serve $MODEL \
         --port 7100 \
         --disable-log-requests \
         --enforce-eager \
         --no-enable-prefix-caching \
         --kv-transfer-config \
-        '{"kv_connector":"LMCacheConnectorV1","kv_role":"kv_producer","kv_connector_extra_config": {"discard_partial_chunks": false, "lmcache_rpc_port": "producer1"}}' \
-        --tensor-parallel-size 2
+        '{"kv_connector":"LMCacheConnectorV1","kv_role":"kv_producer","kv_connector_extra_config": {"discard_partial_chunks": false, "lmcache_rpc_port": "producer1"}}' 
 
 
 
@@ -45,15 +44,14 @@ elif [[ $1 == "decoder" ]]; then
         LMCACHE_CONFIG_FILE=$decode_config_file \
         VLLM_ENABLE_V1_MULTIPROCESSING=1 \
         VLLM_WORKER_MULTIPROC_METHOD=spawn \
-        CUDA_VISIBLE_DEVICES=2,3 \
+        CUDA_VISIBLE_DEVICES=1 \
         vllm serve $MODEL \
         --port 7200 \
         --disable-log-requests \
         --enforce-eager \
         --no-enable-prefix-caching \
         --kv-transfer-config \
-        '{"kv_connector":"LMCacheConnectorV1","kv_role":"kv_consumer","kv_connector_extra_config": {"discard_partial_chunks": false, "lmcache_rpc_port": "consumer1", "skip_last_n_tokens": 1}}' \
-        --tensor-parallel-size 2
+        '{"kv_connector":"LMCacheConnectorV1","kv_role":"kv_consumer","kv_connector_extra_config": {"discard_partial_chunks": false, "lmcache_rpc_port": "consumer1", "skip_last_n_tokens": 1}}'
 
 
 else
