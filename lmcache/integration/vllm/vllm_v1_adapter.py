80b6028696fcc82376484192100bab5ea1bd048f
xshwu <xshwu@tencent.com>
2025-07-16 17:26:53 +0800

    rebase: tx-dev-20250714

--- lmcache/integration/vllm/vllm_v1_adapter.py
+++ lmcache/integration/vllm/vllm_v1_adapter.py
@@ -59,6 +59,92 @@ if TYPE_CHECKING:
 logger = init_logger(__name__)
 
 
+def get_zmq_rpc_path_lmcache(
+    role: KVConnectorRole,
+    is_tp: bool = False,
+    vllm_config: Optional["VllmConfig"] = None,
+) -> str:
+    base_url = envs.VLLM_RPC_BASE_PATH
+    # Default to 0 if not configured
+    rpc_port = 0
+    if vllm_config is not None:
+        rpc_port = vllm_config.kv_transfer_config.get_from_extra_config(
+            "lmcache_rpc_port", 0
+        )
+    logger.debug("Base URL: %s, RPC Port: %s", base_url, rpc_port)
+    return f"ipc://{base_url}/lmcache_rpc_port_{rpc_port}"
+
+
+# TODO: move this to LMCache so that we can gracefully close it
+class LMCacheLookupClient:
+    def __init__(self, role: KVConnectorRole, is_tp: bool, vllm_config: "VllmConfig"):
+        self.encoder = MsgpackEncoder()
+        self.ctx = zmq.Context()  # type: ignore[attr-defined]
+        socket_path = get_zmq_rpc_path_lmcache(role, is_tp, vllm_config)
+        self.socket = make_zmq_socket(
+            self.ctx,
+            socket_path,
+            zmq.REQ,  # type: ignore[attr-defined]
+            bind=False,
+        )
+
+    def lookup(self, token_ids: torch.Tensor) -> int:
+        request = self.encoder.encode(token_ids)
+        self.socket.send_multipart(request, copy=False)
+        resp = self.socket.recv()
+        result = int.from_bytes(resp, "big")
+        return result
+
+    def close(self):
+        self.socket.close(linger=0)
+
+
+class LMCacheLookupServer:
+    def __init__(
+        self,
+        lmcache_engine: LMCacheEngine,
+        role: KVConnectorRole,
+        is_tp: bool,
+        vllm_config: "VllmConfig",
+    ):
+        self.decoder = MsgpackDecoder(torch.Tensor)
+        self.ctx = zmq.Context()  # type: ignore[attr-defined]
+        socket_path = get_zmq_rpc_path_lmcache(role, is_tp, vllm_config)
+        self.socket = make_zmq_socket(
+            self.ctx,
+            socket_path,
+            zmq.REP,  # type: ignore[attr-defined]
+            bind=True,
+        )
+
+        self.lmcache_engine = lmcache_engine
+        self.running = True
+
+        def process_request():
+            while self.running:
+                # try:
+                # request = self.socket.recv()
+                frames = self.socket.recv_multipart(copy=False)
+                token_ids = self.decoder.decode(frames)
+                result = self.lmcache_engine.lookup(token_ids, pin=True)
+                response = result.to_bytes(4, "big")
+                self.socket.send(response)
+                # except Exception as e:
+                #    logger.error("Error in LMCache lookup server: %s", e)
+                #    break
+                # continue
+
+        self.thread = threading.Thread(target=process_request, daemon=True)
+        self.thread.start()
+        logger.info(
+            f"Started LMCache lookup server for tp = {vllm_config.parallel_config.rank}"
+        )
+
+    def close(self):
+        self.socket.close(linger=0)
+        # TODO: close the thread!
+
+
 @dataclass
 class LoadSpec:
     # Number of tokens cached in vLLM
@@ -370,7 +456,7 @@ class LMCacheConnectorV1Impl:
                 vllm_config.parallel_config,
                 vllm_config.cache_config,
                 vllm_config.scheduler_config,
-                vllm_config=vllm_config,
+                vllm_config,
             )
 
             self.use_layerwise = self.config.use_layerwise
