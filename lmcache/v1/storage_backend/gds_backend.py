67665727747d97d28393badb0f141a1eb14a2ceb
ApostaC <yihua98@uchicago.edu>
2025-07-21 17:55:52 -0700

    [fix] linter erros
    Signed-off-by: ApostaC <yihua98@uchicago.edu>

--- lmcache/v1/storage_backend/gds_backend.py
+++ lmcache/v1/storage_backend/gds_backend.py
@@ -672,14 +672,14 @@ class GdsBackend(StorageBackendInterface):
             return size_in_bytes
 
     def pin(self, key: CacheEngineKey) -> bool:
-        # NOTE (ApostaC): Since gds doesn't have eviction now, we don't need 
+        # NOTE (ApostaC): Since gds doesn't have eviction now, we don't need
         # to implement pin and unpin
-        pass
+        return
 
     def unpin(self, key: CacheEngineKey) -> bool:
-        # NOTE (ApostaC): Since gds doesn't have eviction now, we don't need 
+        # NOTE (ApostaC): Since gds doesn't have eviction now, we don't need
         # to implement pin and unpin
-        pass
+        return
 
     def close(self) -> None:
         logger.info("GDS backend closed.")
