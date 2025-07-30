f58bd033f47f9685c0e8686c503b659088e1da8a
Samuel Shen <slshen@uchicago.edu>
2025-07-30 18:03:08 +0000

    bring torch back in without version pinning
    Signed-off-by: Samuel Shen <slshen@uchicago.edu>

--- .buildkite/scripts/install-env.sh
+++ .buildkite/scripts/install-env.sh
@@ -15,7 +15,6 @@ fi
 # CUDA version
 CUDA_VERSION="12.1"
 
-uv pip install torch
 uv pip install --upgrade pip setuptools wheel
 uv pip install -r requirements/common.txt
 uv pip install -r requirements/test.txt
