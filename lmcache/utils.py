96a596cad2c8683c766be66feb57390be7b7f396
Shaoting <shaotingf@uchicago.edu>
2025-08-27 02:09:39 +0000

    Avoid doing int convertion on hash
    Signed-off-by: Shaoting <shaotingf@uchicago.edu>

--- lmcache/utils.py
+++ lmcache/utils.py
@@ -171,7 +171,7 @@ class CacheEngineKey:
             parts[1],
             int(parts[2]),
             int(parts[3]),
-            int(parts[4], 16),
+            parts[4],
             request_configs,
         )
 
