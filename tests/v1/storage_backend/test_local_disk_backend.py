c983211c4214246c2b8fe5e15a400b20d076746a
chickeyton <ngton2014@gmail.com>
2025-08-29 15:12:09 +0800

    fix UT
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- tests/v1/storage_backend/test_local_disk_backend.py
+++ tests/v1/storage_backend/test_local_disk_backend.py
@@ -17,6 +17,7 @@ from lmcache.v1.config import LMCacheEngineConfig
 from lmcache.v1.memory_management import (
     MemoryFormat,
     MemoryObj,
+    MixedMemoryAllocator,
 )
 from lmcache.v1.storage_backend.local_cpu_backend import LocalCPUBackend
 from lmcache.v1.storage_backend.local_disk_backend import LocalDiskBackend
