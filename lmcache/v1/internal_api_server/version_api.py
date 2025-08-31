0c8ca6fe913d7344848b9cb4956e1cd0d3c0270a
baoloongmao <baoloongmao@tencent.com>
2025-08-31 17:20:18 +0800

    Improve to be more efficiency
    Signed-off-by: baoloongmao <baoloongmao@tencent.com>

--- lmcache/v1/internal_api_server/version_api.py
+++ lmcache/v1/internal_api_server/version_api.py
@@ -6,24 +6,31 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+try:
+    # First Party
+    from lmcache import _version  # type: ignore[attr-defined]
 
-@router.get("/version")
-def get_version():
-    try:
-        # First Party
-        from lmcache import _version
+    VERSION = getattr(_version, "__version__", "")
+    COMMIT_ID = getattr(_version, "__commit_id__", "")
+except ImportError:
+    VERSION = ""
+    COMMIT_ID = ""
 
-        return _version.__version__
-    except (ImportError, AttributeError):
-        return ""
+
+@router.get("/lmc_version")
+async def get_lmc_version():
+    return VERSION
 
 
 @router.get("/commit_id")
 def get_commit_id():
-    try:
-        # First Party
-        from lmcache import _version
+    return COMMIT_ID
 
-        return _version.__commit_id__
-    except (ImportError, AttributeError):
-        return ""
+
+@router.get("/version")
+async def get_version():
+    version = get_lmc_version()
+    commit_id = get_commit_id()
+    version_display = version if version else "NA"
+    commit_id_display = commit_id if commit_id else "NA"
+    return f"{version_display}-{commit_id_display}"
