25f002da6d5d2d098a4c82cf34b3929a63571778
ziruiliu <ziliu@ddn.com>
2025-09-04 23:14:23 +0800

    modify assertion more readble
    Signed-off-by: ziruiliu <ziliu@ddn.com>

--- lmcache/v1/storage_backend/local_cpu_backend.py
+++ lmcache/v1/storage_backend/local_cpu_backend.py
@@ -353,9 +353,7 @@ class LocalCPUBackend(AllocatorBackendInterface):
         if memory_objs is not None or not eviction:
             return memory_objs
 
-        assert isinstance(self.memory_allocator, MixedMemoryAllocator) or isinstance(
-            self.memory_allocator, NixlCPUMemoryAllocator
-        ) or isinstance(self.memory_allocator, PagedTensorMemoryAllocator)
+        assert isinstance(self.memory_allocator, (MixedMemoryAllocator, NixlCPUMemoryAllocator, PagedTensorMemoryAllocator))
 
         evict_keys_count = 0
         num_attempts = 0
