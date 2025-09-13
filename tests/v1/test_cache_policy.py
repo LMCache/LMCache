ca1e0638d6d04fe2f99e7093af3ffcd17d1141f8
Yunjiang Jiang <yunjiangster@gmail.com>
2025-09-13 14:13:45 -0700

    [Misc] Add MRU cache policy to v1 (#1560)
    Add MRU cache policy to v1.
    
    Signed-off-by: yunjiangster <yunjiangster@users.noreply.github.com>
    Signed-off-by: royyhuang <roy.y.huang@gmail.com>

--- tests/v1/test_cache_policy.py
+++ tests/v1/test_cache_policy.py
@@ -159,3 +159,49 @@ def test_lfu_with_pin():
     evict_candidates = policy.get_evict_candidates(cache_dict, num_candidates=2)
 
     assert evict_candidates == [key3, key2]
+
+
+def test_mru():
+    policy = get_cache_policy("MRU")
+    cache_dict = policy.init_mutable_mapping()
+    obj1 = DummyMemoryObj()
+    obj2 = DummyMemoryObj()
+    obj3 = DummyMemoryObj()
+    key1 = dumb_cache_engine_key(1)
+    key2 = dumb_cache_engine_key(2)
+    key3 = dumb_cache_engine_key(3)
+
+    cache_dict[key1] = obj1
+    policy.update_on_put(key1)
+    cache_dict[key2] = obj2
+    policy.update_on_put(key2)
+    cache_dict[key3] = obj3
+    policy.update_on_put(key3)
+
+    policy.update_on_hit(key1, cache_dict)
+    evict_candidates = policy.get_evict_candidates(cache_dict, num_candidates=2)
+    # key1 is the most recent, followed by key3.
+    assert evict_candidates == [key1, key3], (evict_candidates, [key1, key3])
+
+
+def test_mru_with_pin():
+    policy = get_cache_policy("MRU")
+    cache_dict = policy.init_mutable_mapping()
+    obj1 = DummyMemoryObj()
+    obj2 = DummyMemoryObj()
+    obj3 = DummyMemoryObj(can_evict=False)  # Pinned object
+    key1 = dumb_cache_engine_key(1)
+    key2 = dumb_cache_engine_key(2)
+    key3 = dumb_cache_engine_key(3)
+
+    cache_dict[key1] = obj1
+    policy.update_on_put(key1)
+    cache_dict[key2] = obj2
+    policy.update_on_put(key2)
+    cache_dict[key3] = obj3
+    policy.update_on_put(key3)
+
+    policy.update_on_hit(key1, cache_dict)
+    evict_candidates = policy.get_evict_candidates(cache_dict, num_candidates=2)
+    # key1 is most recent, followed by key3, but since key3 is pinned, wo go to key2.
+    assert evict_candidates == [key1, key2], (evict_candidates, [key1, key2])
