4fd9a33dbb9472cce5942caede144ade0c1c9666
baoloongmao <baoloongmao@tencent.com>
2025-09-21 22:21:07 +0800

    Address comments
    Signed-off-by: baoloongmao <baoloongmao@tencent.com>

--- lmcache/v1/config.py
+++ lmcache/v1/config.py
@@ -332,11 +332,6 @@ _CONFIG_DEFINITIONS: dict[str, dict[str, Any]] = {
         "env_converter": _to_str_list,
     },
     # Lookup client configurations
-    "lookup_max_retries": {
-        "type": int,
-        "default": 3,
-        "env_converter": int,
-    },
     "lookup_timeout_ms": {
         "type": int,
         "default": 3000,
