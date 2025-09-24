391cec54f8dd79d0af78a1535d6a6eb3c2dacac4
rigginschen <rigginschen@tencent.com>
2025-09-24 11:33:55 +0800

    better format
    Signed-off-by: rigginschen <rigginschen@tencent.com>

--- lmcache/integration/vllm/vllm_v1_adapter.py
+++ lmcache/integration/vllm/vllm_v1_adapter.py
@@ -662,9 +662,7 @@ class LMCacheConnectorV1Impl:
         self._requests_priority: dict[str, int] = {}
 
         # TODO(baoloongmao): Internal api server & plugin framework support dp > 1
-        if (
-            vllm_config.parallel_config.data_parallel_rank_local == 0
-        ):
+        if vllm_config.parallel_config.data_parallel_rank_local == 0:
             # Start internal API server if enabled
             # The enabled check is in the InternalAPIServer constructor
             self.api_server = InternalAPIServer(self)
