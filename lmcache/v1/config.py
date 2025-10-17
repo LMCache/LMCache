88b7194823b78ebd6917d112207a0ffd3a729a54
Samuel Shen <slshen@uchciago.edu>
2025-10-17 04:06:31 +0000

    fix unit test
    Signed-off-by: Samuel Shen <slshen@uchciago.edu>

--- lmcache/v1/config.py
+++ lmcache/v1/config.py
@@ -474,6 +474,15 @@ def _create_config_class():
 def _validate_config(self):
     """Validate configuration"""
 
+    # auto-adjust save_unfull_chunk for async loading to prevent CPU fragmentation
+    if self.enable_async_loading or self.use_layerwise:
+        logger.warning(
+            "Automatically setting save_unfull_chunk=False because "
+            "enable_async_loading=True or use_layerwise=True to prevent "
+            "CPU memory fragmentation"
+        )
+        self.save_unfull_chunk = False
+
     if self.enable_p2p:
         assert self.enable_controller
         assert self.controller_pull_url is not None
