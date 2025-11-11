ed20f67ce68113eb429d1de1ec96f42902192505
ApostaC <yihua98@uchicago.edu>
2025-11-11 22:27:18 +0000

    [Add] cuda messaging futures and unit tests
    Signed-off-by: ApostaC <yihua98@uchicago.edu>

--- lmcache/v1/multiprocess/futures.py
+++ lmcache/v1/multiprocess/futures.py
@@ -3,6 +3,9 @@
 from typing import Generic, Optional, TypeVar
 import threading
 
+# Third Party
+import torch
+
 T = TypeVar("T")
 
 
@@ -53,5 +56,135 @@ class MessagingFuture(Generic[T]):
         return self.result_
 
     def set_result(self, result: T) -> None:
+        """
+        Set the result of the future and mark it as done. This function is NOT
+        SUPPOSED TO BE CALLED by users directly. It should be only called by
+        the messaging system when the result is available.
+
+        Args:
+            result (T): The result to set.
+        """
         self.result_ = result
         self.is_done_.set()
+
+    def to_cuda_future(
+        self,
+        device: torch.cuda.device | None = None,
+    ) -> "CUDAMessagingFuture":
+        # TODO: need extra type checking for the future type
+        return CUDAMessagingFuture.FromMessagingFuture(self, device)  # type: ignore
+
+
+class CUDAMessagingFuture(MessagingFuture[T]):
+    """
+    The future class that wraps both result and a CUDA IPC event.
+    The `query`, `wait`, and `result` methods will pend on both the
+    original future and the CUDA event.
+    The original future should return tuple[bytes, T], where the first
+    element is the serialized CUDA event.
+    """
+
+    def __init__(
+        self,
+        raw_future: MessagingFuture[tuple[bytes, T]],
+        device: torch.cuda.device | None = None,
+    ) -> None:
+        super().__init__()
+        self.raw_future_ = raw_future
+        self.event_: torch.cuda.Event | None = None
+        self.result_: T | None = None
+        self.device_ = device if device is not None else torch.cuda.current_device()
+
+    def _on_raw_future_complete(self):
+        """
+        Update the CUDA event and result when the raw future is complete.
+        """
+        event_bytes, result = self.raw_future_.result()
+        self.result_ = result
+
+        # Deserialize the CUDA event
+        self.event_ = torch.cuda.Event.from_ipc_handle(self.device_, event_bytes)
+
+    def wait(self, timeout: Optional[float] = None) -> bool:
+        """
+        Wait for the future to be done, with the CUDA stream.
+
+        Args:
+            timeout (Optional[float]): Maximum time to wait in seconds.
+                This argument is not supported and must be None.
+
+        Returns:
+            bool: True if the future is done, False if the timeout was reached.
+
+        Raises:
+            ValueError: if the timeout is not None.
+
+        Notes:
+            This function does not support waiting for a specific time.
+        """
+        if timeout is not None:
+            raise ValueError("CUDAMessagingFuture.wait does not support timeout")
+
+        if self.event_:
+            self.event_.synchronize()
+            return True
+
+        flag = self.raw_future_.wait()
+        assert flag, "CUDAMessagingFuture.wait: underlying future failed to complete"
+
+        self._on_raw_future_complete()
+
+        assert self.event_ is not None
+        self.event_.synchronize()
+
+        return True
+
+    def result(self, timeout: Optional[float] = None) -> T:
+        """
+        Get the result of the future.
+
+        Args:
+            timeout (Optional[float]): Maximum time to wait in seconds.
+                This argument is not supported and must be None.
+
+        Returns:
+            T: The result of the future.
+
+        Raises:
+            ValueError: if the timeout is not None.
+        """
+        if timeout is not None:
+            raise ValueError("CUDAMessagingFuture.result does not support timeout")
+
+        self.wait()
+        assert self.result_ is not None
+        return self.result_
+
+    def query(self) -> bool:
+        """
+        Check if the future is done.
+
+        Returns:
+            bool: True if the future is done, False otherwise.
+        """
+        if self.event_:
+            return self.event_.query()
+
+        if self.raw_future_.query():
+            self._on_raw_future_complete()
+            assert self.event_ is not None
+            return self.event_.query()
+
+        return False
+
+    def set_result(self, result: T) -> None:
+        raise NotImplementedError(
+            "CUDAMessagingFuture does not support set_result directly"
+        )
+
+    @staticmethod
+    def FromMessagingFuture(
+        raw_future: MessagingFuture[tuple[bytes, T]],
+        device: torch.cuda.device | None = None,
+    ) -> "CUDAMessagingFuture[T]":
+        return CUDAMessagingFuture(raw_future, device)
