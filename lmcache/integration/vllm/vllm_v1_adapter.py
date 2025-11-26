9c220f488c9fd240811224c61f204dd64a04dca9
Kaizhan Lin <kzlin@siflow.cn>
2025-11-26 16:49:51 +0800

    [Bugfix] subtract computed tokens in fast path cache hit
    Signed-off-by: Kaizhan Lin <kzlin@siflow.cn>

--- lmcache/integration/vllm/vllm_v1_adapter.py
+++ lmcache/integration/vllm/vllm_v1_adapter.py
@@ -1380,7 +1380,18 @@ class LMCacheConnectorV1Impl:
 
         # consult the cache before any processing
         if cached_num_hit_toks := self.lookup_client.lookup_cache(lookup_id=req_id):
-            return cached_num_hit_toks
+            need_to_allocate = max(0, cached_num_hit_toks - num_computed_tokens)
+            
+            if cached_num_hit_toks == request.num_tokens:
+                need_to_allocate = max(0, need_to_allocate - 1)
+
+            self.load_specs[req_id] = LoadSpec(
+                vllm_cached_tokens=num_computed_tokens, 
+                lmcache_cached_tokens=cached_num_hit_toks,
+                can_load=False, 
+            )
+
+            return need_to_allocate
 
         self._requests_priority[req_id] = getattr(request, "priority", 0)
 
