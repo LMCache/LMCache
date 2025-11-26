40db268fb27941725e9cc26d0b96024a33017ecf
Yuhan Liu <32589867+YuhanLiu11@users.noreply.github.com>
2025-11-26 13:47:34 -0800

    [Observability] Adding lifespan tracker (#2073)
    * Adding lifespan tracker
    
    Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>
    
    * Clearing the chunk hash dict to avoid memory leak
    
    Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>
    
    * Fixing comments
    
    Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>
    
    * fixing format checker
    
    Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>
    
    * fixing gemini's comment
    
    Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>
    
    ---------
    
    Signed-off-by: YuhanLiu11 <yliu738@wisc.edu>
    Signed-off-by: aeon-x <talexcao@gmail.com>

--- lmcache/v1/storage_backend/cache_policy/lru.py
+++ lmcache/v1/storage_backend/cache_policy/lru.py
@@ -1,10 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 # Standard
 from collections import OrderedDict
-from typing import Any
+from typing import Any, Dict
+import time
 
 # First Party
 from lmcache.logging import init_logger
+from lmcache.observability import LMCStatsMonitor
 from lmcache.utils import CacheEngineKey
 from lmcache.v1.storage_backend.cache_policy.base_policy import BaseCachePolicy
 
@@ -18,22 +20,37 @@ class LRUCachePolicy(BaseCachePolicy[OrderedDict[CacheEngineKey, Any]]):
 
     def __init__(self):
         logger.info("Initializing LRUCachePolicy")
+        self.chunk_hash_to_init_timestamp: Dict[int, float] = {}
+        self.stats_monitor = LMCStatsMonitor.GetOrCreate()
+        self.max_num_chunk_hash = 12500000
 
     def init_mutable_mapping(self) -> OrderedDict[CacheEngineKey, Any]:
         return OrderedDict()
+
+    def update_chunk_hash_dict(self, key: CacheEngineKey) -> None:
+        curr_time = time.time()
+        key_hash = key.chunk_hash
+        if init_timestamp := self.chunk_hash_to_init_timestamp.get(key_hash, None):
+            time_interval = curr_time - init_timestamp
+            self.stats_monitor.on_chunk_reuse(time_interval)
+        else:
+            if len(self.chunk_hash_to_init_timestamp) >= self.max_num_chunk_hash:
+                self.chunk_hash_to_init_timestamp.clear()
+            self.chunk_hash_to_init_timestamp[key_hash] = curr_time
 
     def update_on_hit(
         self,
         key: CacheEngineKey,
         cache_dict: OrderedDict[CacheEngineKey, Any],
     ) -> None:
+        self.update_chunk_hash_dict(key)
         cache_dict.move_to_end(key)
 
     def update_on_put(
         self,
         key: CacheEngineKey,
     ) -> None:
-        # No action needed for LRU on put, as the key is already at the end.
+        self.update_chunk_hash_dict(key)
         pass
 
     def update_on_force_evict(
