1e9bafd634143a27b68476999cb9854632f00df9
zhengliyuan1987 <zhengliyuan1987@139.com>
2025-11-27 19:31:07 +0800

    Fix a typo issue within observability.py
    Signed-off-by: zhengliyuan1987 <zhengliyuan1987@139.com>

--- lmcache/observability.py
+++ lmcache/observability.py
@@ -1317,7 +1317,7 @@ class PrometheusLogger:
         #    "PrometheusLogger instance already created with different metadata"
         if PrometheusLogger._instance.metadata != metadata:
             logger.error(
-                "PrometheusLogger instance already created with"
+                "PrometheusLogger instance already created with "
                 "different metadata. This should not happen except "
                 "in test"
             )
