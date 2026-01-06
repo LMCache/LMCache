d5d62d580feba1b4ede95cbf22f0e7f1f62cfeb1
Zirui Liu <ziliu@ddn.com>
2026-01-06 16:30:07 +0000

    update server.py's  multi_layer_kv_transfer usage accordingly
    Signed-off-by: Zirui Liu <ziliu@ddn.com>

--- lmcache/v1/multiprocess/server.py
+++ lmcache/v1/multiprocess/server.py
@@ -69,7 +69,6 @@ class GPUCacheContext:
         # MLA shape: [num_blocks, block_size, hidden_dim]
         # MHA shape: [2, num_blocks, block_size, num_heads, head_size]
         self.is_mla_ = self.kv_caches_[0].ndim == 3
-        self.vllm_two_major_ = True
 
         # Shape related
         self.num_layers_ = len(self.kv_caches_)
@@ -78,20 +77,8 @@ class GPUCacheContext:
             self.block_size_ = self.kv_caches_[0].shape[1]
             self.hidden_dim_size_ = self.kv_caches_[0].shape[2]
         else:
-            if self.kv_caches_[0].shape[0] == 2:
-                self.vllm_two_major_ = True
-                self.num_blocks_ = self.kv_caches_[0].shape[1]
-                self.block_size_ = self.kv_caches_[0].shape[2]
-            elif self.kv_caches_[0].shape[1] == 2:
-                self.vllm_two_major_ = False
-                self.num_blocks_ = self.kv_caches_[0].shape[0]
-                self.block_size_ = self.kv_caches_[0].shape[2]
-            else:
-                raise ValueError(
-                    "The kv_caches should have shape "
-                    "[2, num_blocks, block_size, num_heads, head_size] or "
-                    "[num_blocks, 2, block_size, num_heads, head_size]."
-                )
+            self.num_blocks_ = self.kv_caches_[0].shape[1]
+            self.block_size_ = self.kv_caches_[0].shape[2]
             # hidden_dim = num_heads * head_size
             num_heads = self.kv_caches_[0].shape[3]
             head_size = self.kv_caches_[0].shape[4]
@@ -147,13 +134,6 @@ class GPUCacheContext:
         Returns a GPU tensor of the KV cache pointers
         """
         return self.kv_cache_pointers_
-
-    @property
-    def vllm_two_major(self) -> bool:
-        """
-        Returns whether the KV cache uses the [2, num_blocks, ...] layout
-        """
-        return self.vllm_two_major_
 
     @property
     def stream(self) -> torch.cuda.Stream:
@@ -340,10 +320,9 @@ class MPCacheEngine:
                         slot_mapping,
                         gpu_context.device,
                         gpu_context.block_size * gpu_context.num_blocks,
-                        True,
-                        gpu_context.is_mla,
-                        gpu_context.vllm_two_major,
-                        gpu_context.block_size,
+                        lmc_ops.TransferDirection.D2H,
+                        self.kv_layout,
+                        self.block_size,
                     )
 
                     assert memory_obj.tensor is not None
@@ -422,10 +401,9 @@ class MPCacheEngine:
                         slot_mapping,
                         gpu_context.device,
                         gpu_context.block_size * gpu_context.num_blocks,
-                        False,
-                        gpu_context.is_mla,
-                        gpu_context.vllm_two_major,
-                        gpu_context.block_size,
+                        lmc_ops.TransferDirection.H2D,
+                        self.kv_layout,
+                        self.block_size,
                     )
 
         with (
