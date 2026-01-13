7836b738cf4049af288506fb9ce0da99cd4ec89e
Shaoting Feng <shaotingf@uchicago.edu>
2026-01-13 00:04:29 +0000

    Log the transmission time
    Signed-off-by: Shaoting Feng <shaotingf@uchicago.edu>

--- lmcache/v1/storage_backend/pd_backend.py
+++ lmcache/v1/storage_backend/pd_backend.py
@@ -412,10 +412,13 @@ class PDBackend(AllocatorBackendInterface):
 
             # TODO(Jiayi): Consider making this real async
             # Perform the actual transfer
+            start = time.time()
             self.transfer_channel.batched_write(
                 objects=mem_objs_to_send,
                 transfer_spec=channel_transfer_spec,
             )
+            end = time.time()
+            print(f"Sending {len(mem_objs_to_send)} memory objects costs {end - start}s.")
 
             # TODO(Jiayi): consider moving this to the transfer channel
             # since we might want the transfer to be async.
