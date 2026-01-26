41feb77bc724ae20aaedfde2e18672762d4525be
Shaoting-Feng <shaotingf@uchicago.edu>
2026-01-26 22:06:48 +0000

    Add requests to requirements
    Signed-off-by: Shaoting-Feng <shaotingf@uchicago.edu>

--- .buildkite/scripts/vllm-integration-tests.sh
+++ .buildkite/scripts/vllm-integration-tests.sh
@@ -270,7 +270,7 @@ run_pd_lmcache() {
     fi
     source .venv/bin/activate
     uv pip install -r "$ORIG_DIR/requirements/build.txt" > /dev/null 2>&1
-    uv pip install torch==2.7.1 httpx fastapi uvicorn > /dev/null 2>&1
+    uv pip install torch==2.7.1 httpx fastapi uvicorn requests > /dev/null 2>&1
     uv pip install -e "$ORIG_DIR" --no-build-isolation > /dev/null 2>&1
     # Start proxy
     python3 "$ORIG_DIR/examples/disagg_prefill/disagg_proxy_server.py" \
@@ -630,7 +630,7 @@ echo "Using port $PORT to send or receive requests."
 cd docker/
 
 # Create the container image
-build_lmcache_vllmopenai_image
+# build_lmcache_vllmopenai_image
 
 ########
 # MAIN #
