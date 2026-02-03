14757294b3452a911b637dc996035a53e13ded2a
Shaoting Feng <shaotingf@uchicago.edu>
2026-02-03 17:46:00 -0500

    Clean up
    Signed-off-by: Shaoting Feng <shaotingf@uchicago.edu>

--- .buildkite/scripts/vllm-integration-tests.sh
+++ .buildkite/scripts/vllm-integration-tests.sh
@@ -630,7 +630,7 @@ echo "Using port $PORT to send or receive requests."
 cd docker/
 
 # Create the container image
-# build_lmcache_vllmopenai_image
+build_lmcache_vllmopenai_image
 
 ########
 # MAIN #
