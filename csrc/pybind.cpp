d0bbdc716f7c569919553cb776d5586b35eb546c
Samuel Shen <slshen@uchciago.edu>
2026-02-10 05:48:25 +0000

    add keyword bindings and remove flashinfer from correctness tests
    Signed-off-by: Samuel Shen <slshen@uchciago.edu>

--- csrc/pybind.cpp
+++ csrc/pybind.cpp
@@ -27,8 +27,14 @@ PYBIND11_MODULE(c_ops, m) {
   m.def("multi_layer_kv_transfer", &multi_layer_kv_transfer);
   m.def("multi_layer_kv_transfer_unilateral",
         &multi_layer_kv_transfer_unilateral);
-  m.def("single_layer_kv_transfer", &single_layer_kv_transfer);
-  m.def("single_layer_kv_transfer_sgl", &single_layer_kv_transfer_sgl);
+  m.def("single_layer_kv_transfer", &single_layer_kv_transfer,
+        py::arg("lmc_key_value_cache"), py::arg("vllm_key_value_cache"),
+        py::arg("slot_mapping"), py::arg("direction"), py::arg("gpu_kv_format"),
+        py::arg("token_major") = false);
+  m.def("single_layer_kv_transfer_sgl", &single_layer_kv_transfer_sgl,
+        py::arg("lmc_key_value_cache"), py::arg("sgl_key_cache"),
+        py::arg("sgl_value_cache"), py::arg("slot_mapping"),
+        py::arg("direction"), py::arg("token_major") = false);
   m.def("load_and_reshape_flash", &load_and_reshape_flash);
   m.def("reshape_and_cache_back_flash", &reshape_and_cache_back_flash);
   m.def("lmcache_memcpy_async", &lmcache_memcpy_async);
