ff5dcc68ed3fe015178409980af43370722f966a
Samuel Shen <slshen@uchciago.edu>
2026-02-13 18:50:29 +0000

    actually don't need clone
    Signed-off-by: Samuel Shen <slshen@uchciago.edu>

--- tests/v1/test_mem_kernels.py
+++ tests/v1/test_mem_kernels.py
@@ -219,7 +219,7 @@ def test_multi_layer_kernel(num_tokens, gpu_kv_format):
             kv_layer.permute(1, 0, 2, 3, 4).contiguous() for kv_layer in kv_cache
         ]
     else:
-        kv_cache_force_old = kv_cache.clone()
+        kv_cache_force_old = kv_cache
     page_buffer_size = num_blocks * block_size
 
     slot_mapping = random.sample(range(0, num_blocks * block_size), num_tokens)
