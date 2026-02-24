b484c436967ac47de39d17b848ad54c800609de4
maobaolong <baoloongmao@tencent.com>
2026-02-24 10:33:48 +0800

    Using shm to reduce memory copy while using remote connector (#2601)
    * Shm and demo
    
    Signed-off-by: baoloongmao <baoloongmao@tencent.com>
    
    * support set worker addr to verify in container env
    
    Signed-off-by: baoloongmao <baoloongmao@tencent.com>
    
    * Add missing file
    
    Signed-off-by: baoloongmao <baoloongmao@tencent.com>
    
    * Fix unit
    
    Signed-off-by: baoloongmao <baoloongmao@tencent.com>
    
    * Add a shm memory example
    
    Signed-off-by: baoloongmao <baoloongmao@tencent.com>
    
    * Move to right folder
    
    Signed-off-by: baoloongmao <baoloongmao@tencent.com>
    
    * Fix security vulnerabilities based on Gemini code review
    
    - Fix integer overflow vulnerabilities in shm_file_worker by adding robust boundary checks
    - Change default network binding from 0.0.0.0 to 127.0.0.1 for better security
    - Add GIL release for free_shm_pinned_ptr to prevent blocking Python interpreter
    - Move all imports to the top of test files for better code organization
    
    Signed-off-by: baoloongmao <baoloongmao@tencent.com>
    
    * Address comment
    
    Signed-off-by: baoloongmao <baoloongmao@tencent.com>
    
    ---------
    
    Signed-off-by: baoloongmao <baoloongmao@tencent.com>
    Signed-off-by: Aaron Wu <aaron.wu@dell.com>

--- csrc/pybind.cpp
+++ csrc/pybind.cpp
@@ -52,5 +52,9 @@ PYBIND11_MODULE(c_ops, m) {
   m.def("alloc_numa_ptr", &alloc_numa_ptr,
         py::call_guard<py::gil_scoped_release>());
   m.def("free_numa_ptr", &free_numa_ptr);
+  m.def("alloc_shm_pinned_ptr", &alloc_shm_pinned_ptr,
+        py::call_guard<py::gil_scoped_release>());
+  m.def("free_shm_pinned_ptr", &free_shm_pinned_ptr,
+        py::call_guard<py::gil_scoped_release>());
   m.def("get_gpu_pci_bus_id", &get_gpu_pci_bus_id);
 }
