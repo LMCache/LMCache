9d6d99b958ffd01a78cc6dbb749b5762573e5769
liubj77 <liubj77@gmail.com>
2026-03-09 10:47:04 +0800

    format code
    Signed-off-by: liubj77 <liubj77@gmail.com>

--- lmcache/integration/vllm/vllm_v1_adapter.py
+++ lmcache/integration/vllm/vllm_v1_adapter.py
@@ -235,9 +235,9 @@ class RequestTracker:
             raise ValueError(f"Unsupported new_block_ids type {type(new_block_ids)}")
 
         if preempted:
-            assert (
-                all_token_ids is not None
-            ), f"Preempted request {self.req_id} has no all_token_ids"
+            assert all_token_ids is not None, (
+                f"Preempted request {self.req_id} has no all_token_ids"
+            )
             # the block ids will change after preemption
             self.allocated_block_ids = new_block_ids
             # reset the number of saved tokens
@@ -364,9 +364,9 @@ class ReqMeta:
         if tracker.mm_hashes:
             # TODO: Optimize this
             token_ids = torch.tensor(token_ids)
-            assert (
-                tracker.mm_positions is not None
-            ), "tracker got mm_hashes but no mm_positions"
+            assert tracker.mm_positions is not None, (
+                "tracker got mm_hashes but no mm_positions"
+            )
             apply_mm_hashes_to_token_ids(
                 token_ids, tracker.mm_hashes, tracker.mm_positions
             )
@@ -450,9 +450,9 @@ class LMCacheConnectorV1Impl:
 
         # Load and configure LMCache config
         config = lmcache_get_or_create_config()
-        assert isinstance(
-            config, LMCacheEngineConfig
-        ), "LMCache v1 configuration is should be passed for vLLM v1."
+        assert isinstance(config, LMCacheEngineConfig), (
+            "LMCache v1 configuration is should be passed for vLLM v1."
+        )
         self._apply_extra_config(config, vllm_config)
         self.config = config
 
@@ -524,9 +524,9 @@ class LMCacheConnectorV1Impl:
 
             if self.enable_blending:
                 assert self.lmcache_engine is not None
-                assert (
-                    self.lmcache_engine.gpu_connector is not None
-                ), "GPU connector must be available for blending"
+                assert self.lmcache_engine.gpu_connector is not None, (
+                    "GPU connector must be available for blending"
+                )
                 self.blender = LMCBlenderBuilder.get_or_create(
                     ENGINE_NAME,
                     self.lmcache_engine,
@@ -1562,9 +1562,9 @@ class LMCacheConnectorV1Impl:
                     f"This might be due to an unsupported vLLM version."
                 )
             if preempted:
-                assert (
-                    load_spec is not None
-                ), f"Request {req_id} is preempted but was not given a load spec"
+                assert load_spec is not None, (
+                    f"Request {req_id} is preempted but was not given a load spec"
+                )
                 # num_computed_tokens should be reset to 0 during preemption
                 # and then set to the number of already cached tokens (maxxing
                 # prefix caching and lmcache)
