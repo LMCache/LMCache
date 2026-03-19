b72ff7bfd3a3b18970062969fc882a53d4be5f44
YaoJiayi <120040070@link.cuhk.edu.cn>
2026-03-19 20:54:25 +0000

    fix observability
    Signed-off-by: YaoJiayi <120040070@link.cuhk.edu.cn>

--- lmcache/v1/mp_observability/logger/l2_stats_logger.py
+++ lmcache/v1/mp_observability/logger/l2_stats_logger.py
@@ -3,13 +3,13 @@
 from typing import Any, Dict, Optional
 
 # First Party
-from lmcache.v1.distributed.internal_api import L2ManagerListener
+from lmcache.v1.distributed.internal_api import L2AdapterListener
 from lmcache.v1.mp_observability.logger.prometheus_logger import (
     PrometheusLogger,
 )
 
 
-class L2ManagerStatsLogger(L2ManagerListener, PrometheusLogger):
+class L2ManagerStatsLogger(L2AdapterListener, PrometheusLogger):
     def __init__(
         self,
         labels: Optional[Dict[str, str]] = None,
@@ -19,8 +19,16 @@ class L2ManagerStatsLogger(L2ManagerListener, PrometheusLogger):
             labels = {}
         PrometheusLogger.__init__(self, labels=labels, config=config)
 
-    # L2ManagerListener callbacks
-    def on_l2_lookup_and_lock(self):
+    # L2AdapterListener callbacks
+    def on_l2_keys_stored(self, keys):
+        # No-op: L2 metrics will be added when L2 is finalized
+        pass
+
+    def on_l2_keys_accessed(self, keys):
+        # No-op: L2 metrics will be added when L2 is finalized
+        pass
+
+    def on_l2_keys_deleted(self, keys):
         # No-op: L2 metrics will be added when L2 is finalized
         pass
 
