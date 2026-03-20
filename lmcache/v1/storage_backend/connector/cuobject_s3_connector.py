af73f8e23f4df8bbf86e78984569193783abed42
Aaron Wu <julienwuw@hotmail.com>
2026-03-20 21:24:40 +0800

    Update lmcache/v1/storage_backend/connector/cuobject_s3_connector.py
    Signed-off-by: Aaron Wu<julienwuw@hotmail.com>

--- lmcache/v1/storage_backend/connector/cuobject_s3_connector.py
+++ lmcache/v1/storage_backend/connector/cuobject_s3_connector.py
@@ -221,7 +221,7 @@ class CuObjectS3Connector(S3Connector):
         self,
         key_str: str,
         mem_obj: MemoryObj,
-    ):
+    ) -> s3.S3Request:
         """RDMA-accelerated download.
 
         If RDMA is enabled, injects ``x-amz-rdma-token`` into the HTTP
