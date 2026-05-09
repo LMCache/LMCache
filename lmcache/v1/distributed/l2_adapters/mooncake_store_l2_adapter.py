f745de92b03c638f0b5cebf3f297670a583369bb
fangchizheng <fangchizheng@mail.ustc.edu.cn>
2026-05-09 11:22:47 +0800

    [Fix]: Include per_op_workers in native client kwargs for Mooncake L2 adapter
    Signed-off-by: fangchizheng <fangchizheng@mail.ustc.edu.cn>

--- lmcache/v1/distributed/l2_adapters/mooncake_store_l2_adapter.py
+++ lmcache/v1/distributed/l2_adapters/mooncake_store_l2_adapter.py
@@ -240,9 +240,8 @@ def _create_mooncake_store_l2_adapter(
         "config": config.setup_config,
         "num_workers": config.num_workers,
         "l1_registration": l1_registration,
+        "per_op_workers" : config.per_op_workers,
     }
-    if config.per_op_workers is not None:
-        native_client_kwargs["per_op_workers"] = config.per_op_workers
 
     native_client = LMCacheMooncakeClient(**native_client_kwargs)
     logger.info(
