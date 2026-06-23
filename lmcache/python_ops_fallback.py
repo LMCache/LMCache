0a9ee763e6dd3a430f850838e05a05b317a2f97f
Tony Lin <tony.lin@intel.com>
2026-06-23 00:30:12 +0000

    fix code quality
    Signed-off-by: Tony Lin <tony.lin@intel.com>

--- lmcache/python_ops_fallback.py
+++ lmcache/python_ops_fallback.py
@@ -414,6 +414,7 @@ def alloc_pinned_numa_ptr(size: int, numa_id: int = 0) -> int:
 
 def free_pinned_numa_ptr(ptr: int, size: int | None = None) -> None:
     """Non-CUDA equivalent of freeing a previously allocated NUMA pointer."""
+
     # Release the tensor object for that pointer reference
     _tensor_registry.pop(ptr, None)
 
@@ -430,6 +431,7 @@ def alloc_pinned_ptr(size: int, device_id: int = 0) -> int:
 
 def free_pinned_ptr(ptr: int) -> None:
     """Non-CUDA equivalent of freeing a previously allocated pinned pointer."""
+
     # Release the tensor object for that pointer reference
     _tensor_registry.pop(ptr, None)
 
