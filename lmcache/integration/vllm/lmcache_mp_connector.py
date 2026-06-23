69a6831a781a968f11ea25b5ed1d261a1dba7dc4
Ethan Feng <ethan.fengch@gmail.com>
2026-06-24 02:21:11 +0800

    [Bugfix] [MP] fix missing cache_salt in free_lookup_locks call (#3771)
    Signed-off-by: Ethan Feng <ethan.fengch@gmail.com>

--- lmcache/integration/vllm/lmcache_mp_connector.py
+++ lmcache/integration/vllm/lmcache_mp_connector.py
@@ -1015,6 +1015,7 @@ class LMCacheMPConnector(KVConnectorBase_V1, SupportsHMA):
                         start=0,
                         end=free_end,
                         request_id=request.request_id,
+                        cache_salt=tracker.cache_salt,
                     )
                     logger.debug(
                         "Free locks of tokens %d-%d since it is cached by vLLM.",
