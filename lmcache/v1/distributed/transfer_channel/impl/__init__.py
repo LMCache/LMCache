eb5a039978e21f9cf99c11e524941574bf063532
Xianhui <xianhuiwu1@gmail.com>
2026-07-21 10:24:33 +0000

    Add Mooncake P2P backend support
    Signed-off-by: Xianhui <xianhuiwu1@gmail.com>

--- lmcache/v1/distributed/transfer_channel/impl/__init__.py
+++ lmcache/v1/distributed/transfer_channel/impl/__init__.py
@@ -6,4 +6,5 @@ self-registers its factory via ``register_transfer_channel_factory``.
 """
 
 # First Party
+from lmcache.v1.distributed.transfer_channel.impl import mooncake_te_impl  # noqa: F401
 from lmcache.v1.distributed.transfer_channel.impl import nixl_impl  # noqa: F401
