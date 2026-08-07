07c1a939fa2f2ac2983a5b8f4949e026fbf49223
chunxiaozheng <1179548172@qq.com>
2026-08-07 16:17:47 +0800

    bugfix
    Signed-off-by: chunxiaozheng <1179548172@qq.com>

--- lmcache/v1/multiprocess/engine_context.py
+++ lmcache/v1/multiprocess/engine_context.py
@@ -297,6 +297,7 @@ class MPCacheServerContext:
         """
         session = self.session_manager.get_or_create(key.request_id)
         session.set_tokens(list(key.token_ids))
+        session.lookup_ipc_key = key.no_worker_id_version()
         chunk_hashes = [
             TokenHasher.hash_to_bytes(h) for h in session.get_hashes(key.start, key.end)
         ]
