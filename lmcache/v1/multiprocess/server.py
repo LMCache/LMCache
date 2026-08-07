a5a06220036660be380957c59cf9d7d047424d66
Kuntai Du <kuntai@uchicago.edu>
2026-08-08 06:01:54 +0800

    [Telemetry] report L2-related telemetry (#4208)
    Signed-off-by: KuntaiDu <kuntai@uchicago.edu>

--- lmcache/v1/multiprocess/server.py
+++ lmcache/v1/multiprocess/server.py
@@ -14,6 +14,8 @@ import zmq
 # First Party
 from lmcache import torch_dev, torch_device_type
 from lmcache.logging import init_logger
+from lmcache.usage_telemetry.l1_usage import InitializeL1Usage
+from lmcache.usage_telemetry.l2_usage import InitializeL2ConnectorUsage
 from lmcache.usage_telemetry.mp import InitializeMPUsageContext
 from lmcache.usage_telemetry.mp_continuous import InitializeMPContinuousUsage
 from lmcache.v1.distributed.config import (
@@ -401,6 +403,8 @@ def run_cache_server(
 
     InitializeMPUsageContext(mp_config, storage_manager_config)
     InitializeMPContinuousUsage(event_bus, mp_config.chunk_size)
+    InitializeL2ConnectorUsage(event_bus, ctx.storage_manager)
+    InitializeL1Usage(event_bus, ctx.storage_manager)
 
     zmq_context = zmq.Context.instance()
     server = MessageQueueServer(
