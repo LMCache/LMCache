0b51bf3cab3046c428c3b04e4f9661e2a34f55ad
chunxiaozheng <1179548172@qq.com>
2026-08-13 20:33:58 +0800

    update
    Signed-off-by: chunxiaozheng <1179548172@qq.com>

--- lmcache/v1/distributed/l2_adapters/nixl_store_agents/__init__.py
+++ lmcache/v1/distributed/l2_adapters/nixl_store_agents/__init__.py
@@ -1,12 +1,2 @@
 # SPDX-License-Identifier: Apache-2.0
 """Storage-agent implementations used by NIXL L2 adapters."""
-
-# First Party
-from lmcache.v1.distributed.l2_adapters.nixl_store_agents.dynamic_nixl_store_agent import (  # noqa: E501
-    DynamicNixlStorageAgent,
-)
-from lmcache.v1.distributed.l2_adapters.nixl_store_agents.file_dynamic_nixl_store_agent import (  # noqa: E501
-    FileDynamicNixlStorageAgent,
-)
-
-__all__ = ["DynamicNixlStorageAgent", "FileDynamicNixlStorageAgent"]
