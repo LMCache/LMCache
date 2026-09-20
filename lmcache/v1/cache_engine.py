c6da4644e2ad46a023f572e0c160c34189b77fa5
SAD LIFE <indeedintrovert@gmail.com>
2026-09-20 17:24:38 +0530

    fix: replace logging f-strings with lazy formatting
    Signed-off-by: SAD LIFE <indeedintrovert@gmail.com>

--- lmcache/v1/cache_engine.py
+++ lmcache/v1/cache_engine.py
@@ -1283,7 +1283,9 @@ class LMCacheEngine:
         )
         assert None not in memory_objs, "Failed to get memory objects to move"
         logger.debug(
-            f"Trying to send {len(memory_objs)} memory objects to {new_position}"
+            "Trying to send %s memory objects to %s",
+            len(memory_objs),
+            new_position,
         )
 
         # TODO: reduce loops
