14601cc79461f4e57c4007c238e839c5831ff876
baoloongmao <307499405@qq.com>
2026-09-21 21:21:46 +0800

    Remove legacy CUDA platform package
    Signed-off-by: baoloongmao <307499405@qq.com>

--- lmcache/v1/platform/cuda/cache_context.py
+++
@@ -1,5 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-"""Compatibility re-export for legacy CUDA cache-context imports."""
-
-# First Party
-from lmcache.v1.platform.devices.cuda.cache_context import *  # noqa: F403
